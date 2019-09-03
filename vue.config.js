620e80c951c8b4c7f88df793fddc44d7c0223ca3
company <1197798263@qq.com>
2019-09-03 17:41:35 +0800

    9-3

--- vue.config.js
+++ vue.config.js
@@ -52,16 +52,14 @@ const vueConfig = {
   },
 
   devServer: {
-    // development server port 8000
-    port: 8000
-    // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-    //     ws: false,
-    //     changeOrigin: true
-    //   }
-    // }
+    port: 8000,
+    proxy: {
+      '/jeecg-boot': {
+        target: 'http://localhost:8081',
+        ws: false,
+        changeOrigin: true
+      }
+    }
   },
 
   // disable source map in production
