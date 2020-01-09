07d071d9303564f99111ba893e0ca3d89a4cc0df
xieyc <1197798263@qq.com>
2020-01-09 13:29:03 +0800

    系统日志

--- src/views/system/modules/LogModal.vue
+++ src/views/system/modules/LogModal.vue
@@ -12,23 +12,54 @@
     <a-spin :spinning="confirmLoading">
       <a-form :form="form">
         <a-form-item label="登录账号" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <a-input
-            v-decorator="[ 'loginName']"
-          />
+          <a-input v-decorator="[ 'loginName']" />
         </a-form-item>
 
         <a-form-item label="用户名字" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <a-input
-            v-decorator="[ 'userName']"
-          />
+          <a-input v-decorator="[ 'userName']" />
+        </a-form-item>
+
+        <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="访问时间">
+          <a-date-picker showTime format="YYYY-MM-DD HH:mm:ss" v-decorator="[ 'createTime', {}]" />
+        </a-form-item>
+
+        <a-form-item label="访问IP" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'ipAddress']" />
+        </a-form-item>
+
+        <a-form-item label="操作类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'opType']" />
+        </a-form-item>
+
+        <a-form-item label="日志类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'logType']" />
+        </a-form-item>
+
+        <a-form-item label="日志内容" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'logContent']" />
+        </a-form-item>
+
+        <a-form-item label="请求路径" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'requestUrl']" />
+        </a-form-item>
+
+        <a-form-item label="请求方法" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'requestMethod']" />
+        </a-form-item>
+
+        <a-form-item label="请求类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="['requestType']" />
+        </a-form-item>
+
+        <a-form-item label="请求参数" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-textarea v-decorator="['requestParam']" :autosize="{ minRows: 2, maxRows: 10 }"/>
         </a-form-item>
 
         <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="备注">
-          <a-textarea placeholder="请输入备注" v-decorator="['remark', {}]" />
+          <a-textarea v-decorator="['remark']" :autosize="{ minRows: 2, maxRows: 6 }"/>
         </a-form-item>
       </a-form>
     </a-spin>
-
   </a-drawer>
 </template>
 
@@ -37,15 +68,13 @@ import pick from 'lodash.pick'
 import moment from 'moment'
 import Vue from 'vue'
 export default {
-  name: 'UserModal',
+  name: 'LogModal',
   data() {
     return {
-      modalWidth: 800,
+      modalWidth: 1800,
       drawerWidth: 700,
       modaltoggleFlag: true,
       confirmDirty: false,
-      disableSubmit: false,
-      dateFormat: 'YYYY-MM-DD',
       title: '操作',
       visible: false,
       model: {},
@@ -60,60 +89,30 @@ export default {
       wrapperCol: {
         xs: { span: 24 },
         sm: { span: 16 }
-      },
+      }
     }
   },
   methods: {
-    // 调用edit
     edit(record) {
       this.resetScreenSize()
       let that = this
       that.form.resetFields()
-      that.userId = record.sysUserId
       that.visible = true
       that.model = Object.assign({}, record)
       that.$nextTick(() => {
-        that.form.setFieldsValue(
-          pick(
-            this.model,
-            'loginName',
-            'userName',
-            'email',
-            'address',
-            'idCard',
-            'phone',
-            'sort',
-            'state',
-            'remark'
-          )
-        )
+        that.form.setFieldsValue(pick(this.model, 'loginName', 'userName', 'ipAddress', 'opType', 'logType', 'logContent', 'requestMethod','requestUrl','requestParam','requestType', 'remark'))
+        //时间格式化
+        that.form.setFieldsValue({createTime: this.model.createTime ? moment(this.model.createTime) : null})
       })
-    },
-
-    refresh() {
-      this.selectedDepartKeys = []
-      this.checkedDepartKeys = []
-      this.checkedDepartNames = []
-      this.checkedDepartNameString = ''
     },
 
     close() {
       this.$emit('close')
       this.visible = false
-      this.disableSubmit = false
-      this.selectedRole = []
-      this.checkedDepartNames = []
-      this.checkedDepartNameString = ''
-      this.checkedDepartKeys = []
-      this.selectedDepartKeys = []
     },
     moment,
     handleCancel() {
       this.close()
-    },
-    handleConfirmBlur(e) {
-      const value = e.target.value
-      this.confirmDirty = this.confirmDirty || !!value
     },
 
     // 根据屏幕变化,设置抽屉尺寸
